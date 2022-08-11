8fe14c3ce41aac01d52d604883705c6a0818ad1a
Maxi.Barreto <maxi.barreto.alejo@gmail.com>
2022-08-11 18:23:47 -0300

    algoritmos 1

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -7,6 +7,17 @@ function factorear(num) {
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
+  var arr = [1];
+  var divisor = 2
+  while(num !==1){
+    if(num % divisor === 0){
+      arr.push(divisor);
+      num = num / divisor
+    }else{
+      divisor++
+    }
+  }
+  return arr;
 }
 
 function bubbleSort(array) {
@@ -15,7 +26,26 @@ function bubbleSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+
+  // 1 5 4 6 2
+  
+
+  var order = true
+  while(order){
+    order = false
+    for(var i = 0; i < array.length - 1; i++){
+      if(array[i] > array[i+1]){
+        var aux = array[i]
+        array[i] = array[i+1]
+        array[i+1] = aux;
+        order = true;
+      }
+    }
+  }
+return array;
 }
+
+  // 1 5 4 6 2
 
 
 function insertionSort(array) {
@@ -24,7 +54,18 @@ function insertionSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  for(var i = 0; i < array.length; i++){
+    var j = i - 1;
+    var aux = array[i]
+    while(j >= 0 && array[j] > aux){
+      array[j + 1] = array[j];
+      j--
+    }
+    array[j+1] = aux;
+  }
+  return array
 }
+
 
 
 function selectionSort(array) {
@@ -32,8 +73,21 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(var i = 0 ; i < array.length - 1; i++){
+    var min = i
+    for(var k = i; k < array.length; k++){
+      if(array[k] < array[min]){
+      min = k
+      }
+    }
+  
+  var aux = array[i]
+  array[i] = array[min]
+  array[min] = aux;
+  }
+return array;
 }
+
 
 
 // No modificar nada debajo de esta línea
